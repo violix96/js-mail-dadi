6ac84f1ba51a3eb0445b5114afed1bc78c503407
Vincenzo Fabrizio Viola <vincy1621@gmail.com>
2024-05-02 19:10:42 +0200

    [UPD] added of conditional instructions and printing of the score result in the console to check who won

--- Dadi/main_dadi.js
+++ Dadi/main_dadi.js
@@ -14,3 +14,12 @@ console.log("Il numero del Giocatore è: " + numeroGiocatore);
 const numeroComputer = Math.floor(Math.random() * 6 + 1);
 console.log("Il numero del computer è: " + numeroComputer);
 
+// confronta i punteggi per verificare chi ha vinto 
+
+if (numeroGiocatore > numeroComputer) {
+    console.log("Il giocatore ha vinto");
+}else if (numeroComputer > numeroGiocatore){
+    console.log("Il computer ha vinto");
+}else {
+    console.log("Pareggio")
+}
