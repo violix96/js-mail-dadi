edca4390825c0a5eec980747f292186c3fd52dfb
Vincenzo Fabrizio Viola <vincy1621@gmail.com>
2024-05-02 18:19:11 +0200

    [UPD] added verification of player input

--- Dadi/main_dadi.js
+++ Dadi/main_dadi.js
@@ -6,3 +6,11 @@ console.log("Ciao mondo dadi!");
 
 // chiedo al giocatore, tramite un prompt, di scegliere un numero da 1 a 6
 const numeroGiocatore = Number(prompt("Scegli un numero da 1 a 6:"));
+
+// verifico se l'input dell'utente è valido 
+
+if(isNaN(numeroGiocatore) || numeroGiocatore < 1 || numeroGiocatore > 6){
+    console.log("Il numero inserito non è valido");
+}else{
+    console.log("Il numero che hai scelto è: " + numeroGiocatore);
+}
