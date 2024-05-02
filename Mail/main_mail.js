2d5f13aa3a80d5510475b860568b57daf3571d61
Vincenzo Fabrizio Viola <vincy1621@gmail.com>
2024-05-02 17:21:33 +0200

    [UPD] added printing of the result on email checking in the console

--- Mail/main_mail.js
+++ Mail/main_mail.js
@@ -15,12 +15,20 @@ let autorizzato = false;
 
 // controllo se la mail inserita è presente nella lista delle mail autorizzate 
 
-for (let i = 0; i < mailAutorizzate.lenght; i++){
+for (let i = 0; i < mailAutorizzate.length; i++){
 
 if (emailUser === mailAutorizzate[i]){
     autorizzato = true;
 }
 
+}
+
+// stampa in console del risultato sul controllo delle mail 
+
+if (autorizzato){
+    console.log("accesso consentito");
+}else{
+    console.log("accesso negato");
 }
 
 
