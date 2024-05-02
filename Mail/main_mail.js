8eace1c51eb75114d538d54466698c2b221a6749
Vincenzo Fabrizio Viola <vincy1621@gmail.com>
2024-05-02 16:26:34 +0200

    [UPD] added the prompt to allow the user to enter his email

--- Mail/main_mail.js
+++ Mail/main_mail.js
@@ -5,3 +5,8 @@ console.log("Ciao mondo mail!")
 
 // lista delle mail autorizzate 
 const mailAutorizzate =  ["mailvincenzoviola1@prova.it", "mailvincenzoviola2@prova.it", "mailvincenzoviola3@prova.it", "mailvincenzoviola@prova.it", "mailvincenzoviola4@prova.it"]
+
+// chiedo all'utente di inserire la sua mail in un prompt
+
+const emailUser = prompt("Inserisci la tua email:");
+
