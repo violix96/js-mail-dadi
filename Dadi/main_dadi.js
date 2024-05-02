49105a0dfde99c4fa2e96a2d29aaeb807f92213b
Vincenzo Fabrizio Viola <vincy1621@gmail.com>
2024-05-02 18:12:17 +0200

    [UPD] added the prompt request for the player to choose a number from 1 to 6

--- Dadi/main_dadi.js
+++ Dadi/main_dadi.js
@@ -1,4 +1,8 @@
 // reset js
 'use strict';
 
-console.log("Ciao mondo dadi!")
+
+console.log("Ciao mondo dadi!");
+
+// chiedo al giocatore, tramite un prompt, di scegliere un numero da 1 a 6
+const numeroGiocatore = Number(prompt("Scegli un numero da 1 a 6:"));
